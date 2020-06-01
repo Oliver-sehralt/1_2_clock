6752ee240879f96802e0d974a476f7551b257285
Oliver-sehralt <schlueter1895@gmail.com>
2020-06-01 17:45:40 +0200

    third commit

--- main.js
+++ main.js
@@ -23,19 +23,36 @@ var monate = [
     "Samstag"
     ];
 
+  
+    window.onload = setInterval(clock,1000);
 
-    var d = new Date();
+    function clock()
+    {
+	  var d = new Date();
+	  
+	  var date = d.getDate();
+	  
+	  var month = d.getMonth();
+	  month=monate[month];
+	  
+	  var year = d.getFullYear();
+	  
+	  var day = d.getDay();
+	  day=wocheTag[day];
+	  
+	  var hour =d.getHours();
+      var min = d.getMinutes();
+      var sec = d.getSeconds();
+      
+      min = ( min < 10 ? "0" : "" ) + min;
+      sec = ( sec < 10 ? "0" : "" ) + sec;
 
-    document.getElementById("monat").innerHTML = monate[d.getMonth()];
+      var timeOfDay = ( hour < 12 ) ? "AM" : "PM";
+      hour = ( hour > 12 ) ? hour - 12 : hour;
+      hour = ( hour == 0 ) ? 12 : hour;
+	
+	  document.getElementById("date").innerHTML=day+" "+date+" "+month+" "+year;
+      document.getElementById("time").innerHTML=hour+":"+min+":"+sec + " " + timeOfDay;
+      
 
-    var e = new Date();
-
-    document.getElementById("day").innerHTML = wocheTag[d.getDay()];
-
-    document.getElementById("stunde").innerHTML = 10;
-
-    document.getElementById("minuten").innerHTML = 55;
-
-    document.getElementById("sekunden").innerHTML = 30;
-
-    document.getElementById("periode").innerHTML = "AM";
+    }
